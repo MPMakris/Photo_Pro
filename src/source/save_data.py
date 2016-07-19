999dd4a7a4fae011befbea83f09050737fab5c4d
MPMakris <MPMakris@gmail.com>
2016-07-19 13:48:06 -0700

    Third draft and first test run of searching code on full search

--- src/source/save_data.py
+++ src/source/save_data.py
@@ -1,19 +1,25 @@
 """Methods for saving API data to DataFrames and CSV files."""
 import pandas as pd
+import sys
 
 
 def create_dataframe_from_soup_objects(picture_soup_data):
+    """Turn list of soup objects into a list of lists containing the data."""
     columns = picture_soup_data[0].attrs.keys()
     data_content = []
     num_soups = len(picture_soup_data)
     for i, mini_soup in enumerate(picture_soup_data):
-        data_content.append([int(mini_soup.get(name)) for name in columns])
+        data_content.append([mini_soup.get(name) for name in columns])
         if i % (num_soups/10) == 0:
-            print "   ...{:.0f} Percent Complete".format(float(i)/len(picture_soup_data)*100)
-    df = pd.DataFrame(data=picture_data, columns=columns)
-    print "DataFrame COMPLETE"
+            sys.stdout.write("   ...{:.0f} Percent Complete\r".format(float(i)/len(
+                                                        picture_soup_data)*100))
+            sys.stdout.flush()
+    df = pd.DataFrame(data=data_content, columns=columns)
+    print "DataFrame COMPLETE                     "
     return df
 
+
 def save_dataframe(df, filename):
-    df.to_csv(filename, header=True, index=True, index_label=False, mode='wb')
-    print "Data Saved to {}".format(filename.upper())
+    """Save DataFame to CSV."""
+    df.to_csv(filename, header=True, index=True, index_label=False, mode='wb', encoding='utf-8')
+    print "Data Saved to:\n-->{}\n".format(filename.upper())
--- src/source/search_save_pictures_meta.py
+++ src/source/search_save_pictures_meta.py
@@ -1,13 +1,12 @@
 """A Script to make a search request from Flickr API and save data to CSV."""
 import shutil
-import pandas as pd
-import numpy as np
 from flickr_api import get_flickr_keys, search_flicker
 import sys
 from save_data import create_dataframe_from_soup_objects, save_dataframe
 
 
 def get_keywords():
+    """Produce default set of keywords for image search."""
     search_kwds = {'method': 'flickr.photos.search',
                    'api_key': flickr_keys['Key'],
                    'sort': 'relavance',
@@ -31,8 +30,10 @@ if __name__ == '__main__':
     search_kwds = get_keywords()
     # Search for and save picture metadata:
     print "\nBegin Flickr Image Search For {}:".format(search_tag_term.upper())
-    picture_soup_data = search_flicker(search_tag_term, search_kwds, max_pages_to_download)
-    print "Creating DataFrame..."
+    picture_soup_data = search_flicker(search_tag_term, search_kwds,
+                                       max_pages_to_download)
+    print "Creating DataFrame:"
     dataframe = create_dataframe_from_soup_objects(picture_soup_data)
-    filename = 'data/tables/flickr_image_search_for_' + search_tag_term.upper() + '.csv' 
+    filename = 'data/tables/flickr_image_search_for_' \
+               + search_tag_term.upper() + '.csv'
     save_dataframe(dataframe, filename)
