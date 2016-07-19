3dabb32e135ce80b01aac87bff03ac0809b0e993
MPMakris <MPMakris@gmail.com>
2016-07-19 11:18:44 -0700

    Complete first draft of sourcing script

--- src/source/flickr_api.py
+++ src/source/flickr_api.py
@@ -1,4 +1,6 @@
 """Saved methods for Flickr API use."""
+import requests
+from bs4 import BeautifulSoup
 
 
 def get_flickr_keys(filename='flickr.keys'):
@@ -12,3 +14,45 @@ def get_flickr_keys(filename='flickr.keys'):
         pair = key.split(":")
         flickr_keys[pair[0]] = pair[1]
     return flickr_keys
+
+
+def search_flicker(search_tag_term, search_kwds):
+    """Search Flicker API for photos including a tagged term."""
+    picture_soup_data = []
+    search_kwds['tags'] = search_tag_term
+
+    # Run a first pass to test results.
+    soup, status = flickr_search_results_page(search_tag_term, 1, search_kwds)
+    if status == '200':
+        picture_soup_data.extend(soup.findAll('photo'))
+        total_pages = int(soup.photos.get('pages'))
+        print "Begining Search for '{}': {} Pages Found".format(
+                                                            search_tag_term,
+                                                            total_pages)
+
+    # Continue with remaining pages if first pass ok.
+    if status == '200' and total_pages > 1:
+        for i in range(2, total_pages+1):
+            soup, status = flickr_search_results_page(search_tag_term, i,
+                                                      search_kwds)
+            if int(status) != 200:
+                print "Error on Page {}: Status Code {}".format(i, status)
+                print "Print soup to determine problem."
+                break
+            picture_soup_data.extend(soup.findAll('photo'))
+            if i % 50 == 0:
+                print "{} Pages Complete\r".format(i),
+    print "Search Complete"
+    print "-"*30
+    return picture_soup_data
+
+
+def flickr_search_results_page(search_tag_term, page_num, search_kwds):
+    """Return a single API photo search results page."""
+    search_url = 'https://api.flickr.com/services/rest/'
+    search_kwds['page'] = page_num
+
+    r = requests.get(search_url, params=search_kwds)
+    soup = BeautifulSoup(r.text, 'lxml')
+
+    return soup, r.status_code
--- src/source/search_save_pictures_meta.py
+++ src/source/search_save_pictures_meta.py
@@ -4,31 +4,14 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
-from src.source.flickr_api import get_flickr_keys
+from flickr_api import get_flickr_keys, search_flicker, flickr_search_results_page
 import sys
 
 
-def search_flicker(search_tag_term, search_kwds):
-    """Search Flicker API for photos including a tagged term."""
-    soup, status = flickr_search_results_page(search_tag_term, 1, search_kwds)
-    total_pages = int(soup.photos.get('pages'))
-    return None
-
-
-def flickr_search_results_page(search_tag_term, page_num, search_kwds):
-    """Return a single API photo search results page."""
-    search_url = 'https://api.flickr.com/services/rest/'
-    search_kwds['page'] = page_num
-    search_kwds['tags'] = search_tag_term
-
-    r = requests.get(search_url, params=search_kwds)
-    soup = BeautifulSoup(r.text, 'lxml')
-
-    return soup, r.status_code
 
 if __name__ == '__main__':
     search_tag_term = sys.argv[0]
-    flickr_keys = get_flickr_keys('flickr.keys')
+    flickr_keys = get_flickr_keys('../../flickr.keys')
 
     search_kwds = {'method': 'flickr.photos.search',
                    'api_key': flickr_keys['Key'],
@@ -38,8 +21,5 @@ if __name__ == '__main__':
                    'extras': 'license, date_upload, date_taken, owner_name, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, path_alias, url_o'
                    }
 
-    search_flicker(search_tag_term, search_kwds)
-    # Run Initial Request to Test Search Term
-
-
-    # Continue with Remaining Pages
+    picture_soup_data = search_flicker(search_tag_term, search_kwds)
+    print "DONE!"
