93472304366ad48839de8688603efb790908982b
MPMakris <MPMakris@gmail.com>
2016-07-25 23:21:11 -0700

    attempt at big changes to meta data download to increase unique results

--- src/source/flickr_api.py
+++ src/source/flickr_api.py
@@ -2,68 +2,109 @@
 import requests
 from bs4 import BeautifulSoup
 import sys
+import datetime
+
+
+def get_flickr_url():
+    """Return Flicker API url."""
+    return 'https://api.flickr.com/services/rest/'
 
 
 def get_flickr_keys(filename='flickr.keys'):
     """Get the Ficker user and app keys from the .keys file."""
-    keys_list = []
     flickr_keys = {}
     with open(filename) as f_open:
-        for line in f_open:
-            keys_list.append(line.strip('\n'))
-    for key in keys_list:
+        key_list = f_open.read().split('\n')[0:2]
+    for key in key_list:
         pair = key.split(":")
         flickr_keys[pair[0]] = pair[1]
     return flickr_keys
 
 
-def search_flicker(search_tag_term, search_kwds, max_pages_to_download=None):
-    """Search Flicker API for photos including a tagged term."""
-    picture_soup_data = []
-    search_kwds['tags'] = search_tag_term
-
-    # Run a first pass to test results.
-    soup, status = flickr_search_results_page(search_tag_term, 1, search_kwds)
+def print_download_status(status, count):
+    """Print an updating status message."""
     if int(status) == 200:
-        picture_soup_data.extend(soup.findAll('photo'))
-        total_pages = int(soup.photos.get('pages'))
-        print "Search Results:  {} Pages Found".format(total_pages)
+        sys.stdout.write("(----- {} Pages Downloaded -----)\r".format(count))
+        sys.stdout.flush()
+    else:
+        print "Error on Page {}: Status Code {}".format(count, status)
+        print "Print soup to determine problem.\n"
+
+
+def get_info_from_result(soup):
+    """Determine quantity of pages in search result."""
+    pages = int(soup.photos.get('pages'))
+    results = int(soup.photos.get('total'))
+    return pages, results
+
+
+def flickr_search(keywords, page_num=1):
+    """Make a search of the Flickr API for photos matching [keywords] dict."""
+    keywords['page'] = page_num
+    search_url = get_flickr_url()
+    r = requests.get(search_url, params=keywords)
+    return BeautifulSoup(r.text, 'lxml'), r.status_code
+
+
+def flickr_search_recursive_time(picture_soup, keywords, time_start, time_end,
+                                 page_count=0):
+    """Return a subset of the API search based on date range."""
+    # Set the Time Bounds
+    keywords['min_upload_date'] = time_start
+    keywords['max_upload_date'] = time_end
+    num_results_per_page = keywords['per_page']
+    soup, status = flickr_search(keywords, page_num=1)
+    num_pages, num_results = get_info_from_result(soup)
+    if num_results <= 4000:
+        # If Less than 4000 Results, Proceed With Download
+        for page in range(1, num_results/num_results_per_page+1+1):
+            page_count += 1
+            soup, status = flickr_search(keywords, page)
+            print_download_status(status, page_count)
+            if int(status) == 200:
+                picture_soup.extend(soup.findAll('photo'))
+            else:
+                continue
+    else:
+        # If More than 4000 Results, Split the Time Period in Half
+        time_mid = time_end - time_start
+        picture_soup.extend(flickr_search_recursive_time(
+                    picture_soup, keywords, time_start, time_mid, page_count))
+        picture_soup.extend(flickr_search_recursive_time(
+                    picture_soup, keywords, time_mid+1, time_end, page_count))
+    return picture_soup
+
+
+def search_flicker_complete(search_term, search_kwds, max_pages=None):
+    """Search Flicker API for photos including a tagged term."""
+    # Add Search Term to Keywords
+    search_kwds['tags'] = search_term
+    search_kwds['text'] = search_term
+    # Add Time Limits to Keywords
+    time_range_start = datetime.datetime(1970, 1, 1, 0, 0).strftime('%s')
+    time_range_end = datetime.datetime(2016, 7, 1, 0, 0).strftime('%s')
+    # Empty Object to Store All Incoming Data:
+    picture_soup_data = []
+    # Run a first pass to test results:
+    soup, status = flickr_search(search_kwds, 1)
+    if int(status) == 200:
+        total_pages, total_results = get_info_from_result(soup)
+        print "Search Results:  {} Pages Found | {} Images Found".format(
+                                                    total_pages, total_results)
     else:
         print "Error: \tStatus Code {}".format(status)
         return None
-
-    if max_pages_to_download is not None:
-        if max_pages_to_download < total_pages:
-            print "Downloading First {} Pages of Results".format(
-                                                            max_pages_to_download)
-            total_pages = max_pages_to_download
+    # Display Information to User
+    if max_pages is not None:
+        if max_pages < total_pages:
+            print "Downloading First {} Pages of Results".format(max_pages)
+            total_pages = max_pages
     else:
         print "Downloading All Pages"
-
-    # Continue with remaining pages if first pass ok.
-    if int(status) == 200 and total_pages > 1:
-        for i in range(2, total_pages+1):
-            soup, status = flickr_search_results_page(search_tag_term, i,
-                                                      search_kwds)
-            if int(status) != 200:
-                print "Error on Page {}: Status Code {}".format(i, status)
-                print "Print soup to determine problem."
-                break
-            picture_soup_data.extend(soup.findAll('photo'))
-            if i % 1 == 0:
-                sys.stdout.write("(----- {} Pages Downloaded -----)\r".format(i))
-                sys.stdout.flush()
-                # print ""
+    # Begin Recursive Search through Time Chunks to Get all Pages
+    picture_soup_data = flickr_search_recursive_time(picture_soup_data,
+                                                     search_kwds,
+                                                     time_range_start,
+                                                     time_range_end)
     print "Download COMPLETE                          \n"
     return picture_soup_data, total_pages
-
-
-def flickr_search_results_page(search_tag_term, page_num, search_kwds):
-    """Return a single API photo search results page."""
-    search_url = 'https://api.flickr.com/services/rest/'
-    search_kwds['page'] = page_num
-
-    r = requests.get(search_url, params=search_kwds)
-    soup = BeautifulSoup(r.text, 'lxml')
-    # print r.url
-    return soup, r.status_code
--- src/source/search_save_pictures_meta.py
+++ src/source/search_save_pictures_meta.py
@@ -1,14 +1,14 @@
 """A Script to make a search request from Flickr API and save data to CSV."""
-from flickr_api import get_flickr_keys, search_flicker
+from flickr_api import get_flickr_keys, search_flicker_complete
 import sys
 from save_data import create_dataframe_from_soup_objects, save_dataframe
 from common.os_interaction import check_folder_exists
 
 
-def get_keywords():
+def get_keywords(api_keys):
     """Produce default set of keywords for image search."""
     search_kwds = {'method': 'flickr.photos.search',
-                   'api_key': flickr_keys['Key'],
+                   'api_key': api_keys['Key'],
                    'sort': 'relevance',
                    'content_type': '1',
                    'per_page': '500',
@@ -17,6 +17,28 @@ def get_keywords():
                               o_dims, views, url_o, date_upload, date_taken'
                    }
     return search_kwds
+
+
+def main(search_term, max_pages):
+    """Main function called during script."""
+    # Get api_keys and keywords to search:
+    flickr_keys = get_flickr_keys('flickr.keys')
+    search_kwds = get_keywords(flickr_keys)
+    # Search for and save picture metadata:
+    print "\nBegin Flickr Image Search For {}:".format(search_term.upper())
+    picture_soup, pages_searched = search_flicker_complete(search_term,
+                                                           search_kwds,
+                                                           max_pages)
+    print "Creating DataFrame:"
+    dataframe = create_dataframe_from_soup_objects(picture_soup)
+    # Check for and create if necessary the destination folder.
+    check_folder_exists("data/tables/" + search_term.upper())
+    # Save all data to file.
+    filename = 'data/tables/' + search_term.upper()\
+               + '/flickr_image_search_for_' \
+               + search_term.upper() + '_' + str(pages_searched)\
+               + '.csv'
+    save_dataframe(dataframe, filename)
 
 if __name__ == '__main__':
     """
@@ -33,21 +55,4 @@ if __name__ == '__main__':
     if len(sys.argv) == 2:
         print "\nWarning: [max_pages] Not Specified"
         print "--> Download Size May Be Large"
-    # Get api_keys and keywords to search:
-    flickr_keys = get_flickr_keys('flickr.keys')
-    search_kwds = get_keywords()
-    # Search for and save picture metadata:
-    print "\nBegin Flickr Image Search For {}:".format(search_tag_term.upper())
-    picture_soup_data, pages_searched = search_flicker(search_tag_term,
-                                                       search_kwds,
-                                                       max_pages_to_download)
-    print "Creating DataFrame:"
-    dataframe = create_dataframe_from_soup_objects(picture_soup_data)
-    # Check for and create if necessary the destination folder.
-    check_folder_exists("data/tables/" + search_tag_term.upper())
-    # Save all data to file.
-    filename = 'data/tables/' + search_tag_term.upper()\
-               + '/flickr_image_search_for_' \
-               + search_tag_term.upper() + '_' + str(pages_searched)\
-               + '.csv'
-    save_dataframe(dataframe, filename)
+    main(search_tag_term, max_pages_to_download)
