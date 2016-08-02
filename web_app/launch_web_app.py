db3167ebdac95ca1b68d7190c4b85d8f3df5c967
MPMakris <MPMakris@gmail.com>
2016-08-02 00:22:18 -0700

    edits to web app

--- web_app/launch_web_app.py
+++ web_app/launch_web_app.py
@@ -1,5 +1,18 @@
-"""Script for launching and maintaing the web page."""
-from flask import Flask
+#  -*- coding: utf-8 -*-
+
+"""
+    PhotoPro.science
+    ~~~~~~
+    A photography analytics project completed as a capstone for the Galvanize
+    Data Science Immersive project.
+
+    :copyright: (c) 2015 by Michael Makris.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from flask import (Flask, request, session, g, redirect, url_for, abort,
+                   render_template, flash, Response)
+#  Creat the Applicaiton:
 app = Flask(__name__)
 
 
@@ -7,8 +20,7 @@ app = Flask(__name__)
 @app.route('/')
 def landing_page():
     """Display the landing page."""
-    with open('index.html') as indx:
-        return indx.read()
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
