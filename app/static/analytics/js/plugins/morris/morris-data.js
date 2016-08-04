4b0cd2e348d3da0847f96f84e9d1567ccb5091dd
MPMakris <MPMakris@gmail.com>
2016-08-04 10:30:44 -0700

    work on flask

--- app/static/analytics/js/plugins/morris/morris-data.js
+++ app/static/analytics/js/plugins/morris/morris-data.js
@@ -6,25 +6,62 @@ $(function() {
     Morris.Area({
         element: 'morris-area-chart-all-categories-image-views',
         data: [{
-            views: '30',
-            users: 30
+            period: '2010 Q1',
+            iphone: 2666,
+            ipad: null,
+            itouch: 2647
         }, {
-            views: '30',
-            users: 30
+            period: '2010 Q2',
+            iphone: 2778,
+            ipad: 2294,
+            itouch: 2441
         }, {
-            views: '30',
-            users: 30
+            period: '2010 Q3',
+            iphone: 4912,
+            ipad: 1969,
+            itouch: 2501
         }, {
-            views: '30',
-            users: 30
+            period: '2010 Q4',
+            iphone: 3767,
+            ipad: 3597,
+            itouch: 5689
+        }, {
+            period: '2011 Q1',
+            iphone: 6810,
+            ipad: 1914,
+            itouch: 2293
+        }, {
+            period: '2011 Q2',
+            iphone: 5670,
+            ipad: 4293,
+            itouch: 1881
+        }, {
+            period: '2011 Q3',
+            iphone: 4820,
+            ipad: 3795,
+            itouch: 1588
+        }, {
+            period: '2011 Q4',
+            iphone: 15073,
+            ipad: 5967,
+            itouch: 5175
+        }, {
+            period: '2012 Q1',
+            iphone: 10687,
+            ipad: 4460,
+            itouch: 2028
+        }, {
+            period: '2012 Q2',
+            iphone: 8432,
+            ipad: 5713,
+            itouch: 1791
         }],
-        xkey: 'views',
-        ykey: ['users'],
-        labels: ['users']
-        pointSize: 3,
+        xkey: 'period',
+        ykeys: ['iphone', 'ipad', 'itouch'],
+        labels: ['iPhone', 'iPad', 'iPod Touch'],
+        pointSize: 2,
         hideHover: 'auto',
-        resize: true,
-        behaveLikeLine: true
+        resize: true
     });
 
     // Donut Chart
