5fd0ed8ca47bae81abb62c5d10c0ae0c90aed5c1
MPMakris <MPMakris@gmail.com>
2016-08-04 16:24:09 -0700

    edit app

--- app/launch_web_app.py
+++ app/launch_web_app.py
@@ -72,7 +72,7 @@ if __name__ == "__main__":
         pass
     pdb.set_trace()
     all_images_prepper = open_prepper(
-                         'scripts/data/store/data_prepper_ALL-CATEGORIES.pkl')
+                         '../data/store/data_prepper_ALL-CATEGORIES.pkl')
     image_data, owner_data, pro_data = read_user_and_image_views(
                                                            all_images_prepper)
     # image_views.tolist()
