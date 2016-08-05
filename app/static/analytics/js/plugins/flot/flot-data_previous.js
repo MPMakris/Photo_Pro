a5ebf0094be904075cf181632e713c94aae35c38
MPMakris <MPMakris@gmail.com>
2016-08-04 20:11:12 -0700

    fix web app

---
+++ app/static/analytics/js/plugins/flot/flot-data_previous.js
@@ -0,0 +1,36 @@
+
+//Image Views Per Image
+$(document).ready(function() {
+    console.log("document ready");
+    var offset = 0;
+    plot(bar_numbers);
+
+    function plot(bar_numbers) {
+
+      var barOptions = {
+          series: {
+              bars: {
+                  show: true,
+                  barWidth: 43200000
+              }
+          },
+          xaxis: {
+              mode: "time",
+              timeformat: "%m/%d",
+              minTickSize: [1, "day"]
+          },
+          grid: {
+              hoverable: false
+          },
+          legend: {
+              show: false
+          },
+          tooltip: false
+      };
+      var barData = {
+          label: "bar",
+          data: bar_numbers
+      };
+      var plotObj = $.plot($("#flot-bar-chart-image-results-1"), [barData], barOptions);
+    }
+});
