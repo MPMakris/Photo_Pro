234a8658cc9f6e1bea0fe80a711522b0f875e1b3
MPMakris <MPMakris@gmail.com>
2016-08-05 15:38:20 -0700

    app changes

--- app/static/analytics/js/plugins/flot/flot-data_overview.js
+++ app/static/analytics/js/plugins/flot/flot-data_overview.js
@@ -108,7 +108,6 @@ $(document).ready(function() {
             },
             tooltip: true,
             tooltipOpts: {
-                // content: "%p.0%, %s", // show percentages, rounding to 2 decimal places
                 shifts: {
                     x: 20,
                     y: 0
