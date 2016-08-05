5aa54c17df3a9c4201efdee19ab04e9cabc5db5c
MPMakris <MPMakris@gmail.com>
2016-08-04 23:55:02 -0700

    web app work

--- app/static/analytics/js/plugins/flot/flot-data_previous.js
+++ app/static/analytics/js/plugins/flot/flot-data_previous.js
@@ -39,49 +39,48 @@ $(document).ready(function() {
           //tooltip: false
       };
       var barData = {
-          label: "bar",
+          label: "Probabilities",
           data: ivq_proba
       };
       var plotObj = $.plot($("#flot-bar-chart-image-results-1"), [barData], barOptions);
     }
-});
 
- //User Total Views
-$(function plot_user_views(uvq_proba) {
+    function plot_user_views(uvq_proba) {
 
-  var barOptions = {
-      series: {
-          bars: {
+      var barOptions = {
+          series: {
+              bars: {
+                  show: true,
+                  barWidth: 0.5
+              }
+          },
+          xaxis: {
               show: true,
-              barWidth: 0.5
+              mode: null,
+              min: 0,
+              max: 10,
+              tickDecimals: 3
+          },
+          yaxis: {
+              show: true,
+              mode: null,
+              min: 0,
+              max: 1,
+              tickDecimals: 0
+          },
+          grid: {
+              hoverable: true,
+              autoHighlight: true
+          },
+          legend: {
+              show: true
           }
-      },
-      xaxis: {
-          show: true,
-          mode: null,
-          min: 0,
-          max: 10,
-          tickDecimals: 3
-      },
-      yaxis: {
-          show: true,
-          mode: null,
-          min: 0,
-          max: 1,
-          tickDecimals: 0
-      },
-      grid: {
-          hoverable: true,
-          autoHighlight: true
-      },
-      legend: {
-          show: true
-      }
-      //tooltip: false
-  };
-  var barData = {
-      label: "bar",
-      data: uvq_proba
-  };
-  var plotObj = $.plot($("#flot-bar-chart-user-results-1"), [barData], barOptions);
+          //tooltip: false
+      };
+      var barData = {
+          label: "Probabilities",
+          data: uvq_proba
+      };
+      var plotObj = $.plot($("#flot-bar-chart-user-results-1"), [barData], barOptions);
+    }
 });
